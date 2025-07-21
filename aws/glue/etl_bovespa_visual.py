d68dbc529e6c6afba20388150be0bb30a5a0761a
Manoel Sa <mmessias.sa@gmail.com>
2025-07-21 19:50:24 -0300

    Ajuste em Glue Visual

--- aws/glue/etl_bovespa_visual.py
+++ aws/glue/etl_bovespa_visual.py
@@ -38,23 +38,27 @@ SqlQuery0 = '''
 select m.*, 
 cast(replace(part,",",".")as decimal(10,3))as part,
 cast(replace(partAcum,",",".")as decimal(10,3))as partAcum,
-cast(replace(theoricalQty,".","") as bigint)as theoricalQty
+cast(replace(theoricalQty,".","") as bigint)as theoricalQty,
+year(dt_ref) as ano_ref,
+month(dt_ref) as mes_ref,
+quarter(dt_ref) as trimestre,
+datediff(current_date(), dt_ref) as dias_processamento
 from myDataSource m
 '''
 SQLQuery_node1749692597878 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":DropDuplicates_node1749691095632}, transformation_ctx = "SQLQuery_node1749692597878")
 
 # Script generated for node Change Schema
-ChangeSchema_node1749691305107 = ApplyMapping.apply(frame=SQLQuery_node1749692597878, mappings=[("segment", "string", "setor", "string"), ("cod", "string", "codigo", "string"), ("asset", "string", "acao", "string"), ("type", "string", "tipo", "string"), ("part", "decimal", "part", "double"), ("partAcum", "decimal", "part_acum", "double"), ("theoricalQty", "long", "qtd_teorica", "bigint"), ("dt_ref", "string", "dt_ref", "date")], transformation_ctx="ChangeSchema_node1749691305107")
+ChangeSchema_node1749691305107 = ApplyMapping.apply(frame=SQLQuery_node1749692597878, mappings=[("segment", "string", "setor", "string"), ("cod", "string", "codigo", "string"), ("asset", "string", "acao", "string"), ("type", "string", "tipo", "string"), ("part", "decimal", "part", "double"), ("partAcum", "decimal", "part_acum", "double"), ("theoricalQty", "long", "qtd_teorica", "bigint"), ("dt_ref", "string", "dt_ref", "date"), ("trimestre", "int", "trimestre", "int"), ("ano_ref", "int", "ano_ref", "int"), ("mes_ref", "int", "mes_ref", "int"), ("dias_processamento", "int", "dias_processamento", "int")], transformation_ctx="ChangeSchema_node1749691305107")
 
 # Script generated for node Aggregate
-Aggregate_node1749691185039 = sparkAggregate(glueContext, parentFrame = ChangeSchema_node1749691305107, groups = ["setor", "acao", "dt_ref", "codigo"], aggs = [["qtd_teorica", "sum"]], transformation_ctx = "Aggregate_node1749691185039")
+Aggregate_node1749691185039 = sparkAggregate(glueContext, parentFrame = ChangeSchema_node1749691305107, groups = ["setor", "acao", "dt_ref", "codigo", "trimestre", "ano_ref", "mes_ref", "dias_processamento"], aggs = [["qtd_teorica", "sum"]], transformation_ctx = "Aggregate_node1749691185039")
 
 # Script generated for node Rename Field
 RenameField_node1749693557003 = RenameField.apply(frame=Aggregate_node1749691185039, old_name="`sum(qtd_teorica)`", new_name="soma_qtd_teorica", transformation_ctx="RenameField_node1749693557003")
 
 # Script generated for node Amazon S3
 AmazonS3_node1749691627621 = glueContext.getSink(path="s3://bck-bovespa/refined/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=["dt_ref", "codigo"], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1749691627621")
-AmazonS3_node1749691627621.setCatalogInfo(catalogDatabase="catalog",catalogTableName="bovespa")
+AmazonS3_node1749691627621.setCatalogInfo(catalogDatabase="default",catalogTableName="bovespa")
 AmazonS3_node1749691627621.setFormat("glueparquet", compression="snappy")
 AmazonS3_node1749691627621.writeFrame(RenameField_node1749693557003)
 job.commit()
